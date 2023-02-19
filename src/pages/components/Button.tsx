111f25f7f1bcc9573fa204a6adde050b3d0d85c5
Ceïf <cecece775@gmail.com>
2023-02-19 21:09:16 +0100

    update of a lot of logics

---
+++ src/pages/components/Button.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface Props {
+  text: string;
+  tailwindProps?: string;
+  size?: string;
+  disabled?: boolean;
+  onClick: () => void;
+  position?: string;
+}
+type SizeClasses = {
+  small: string;
+  medium: string;
+  large: string;
+};
+const BasicButton: React.FC<Props> = ({
+  text,
+  tailwindProps = "",
+  size = "medium",
+  disabled = false,
+  onClick,
+  position = "",
+}) => {
+  const sizes: SizeClasses = {
+    small: "py-1 px-3 text-xs",
+    medium: "py-2 px-4 text-sm",
+    large: "py-3 px-5 text-lg",
+  };
+  return (
+    <div
+      className={"z-10 h-10 w-28 content-center items-center justify-center"}
+    >
+      <button
+        onClick={onClick}
+        className={` ${tailwindProps} ${position}   text-center font-semibold text-white focus:outline-none ${
+          sizes[size]
+        } ${disabled ? "cursor-not-allowed opacity-50" : "hover:shadow-lg"}`}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+export default BasicButton;
